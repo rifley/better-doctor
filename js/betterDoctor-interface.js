c08aff9f2e22f91acc2ec21e482b941da0f578ed
Riley Watts <rileywatts.yes@gmail.com>
2017-04-21 15:07:32 -0700

    add print to html functionality for doctor information matching search parameters

--- js/betterDoctor-interface.js
+++ js/betterDoctor-interface.js
@@ -1,6 +1,15 @@
 var Doctor = require('./../js/betterDoctor.js').Doctor;
 var Patient = require('./../js/betterDoctor.js').Patient;
 
+var Patients = [];
+
+function print(firstName, lastName, picture) {
+  $("#output").append(
+    "<div class='doctor'>" +
+    "<img src='"+picture+"' alt='test' class='img-circle'><br>" +
+    "<p>" + lastName + ', ' + firstName + "</p><br>" + "</div>");
+}
+
 
 $(function(){
 
@@ -13,16 +22,11 @@ $(function(){
     var patientIllness = $("#patientIllness").val();
 
     var newPatient = new Patient(patientFirst, patientLast, patientInsurance, patientIllness);
+    newPatient.getDoctors(newPatient, print);
 
-    var newDoctor = new Doctor(newPatient);
     console.log(newPatient);
-    console.log(newDoctor);
-
+    Patients.push(newPatient);
   });
 
-  var ogDoctor = new Doctor();
-
-
-  ogDoctor.getDoctors("fever");
 
 });
--- js/betterDoctor.js
+++ js/betterDoctor.js
@@ -6,23 +6,40 @@ function Patient(firstName, lastName, insurance, illness) {
   this.lastName = lastName;
   this.insurance = insurance;
   this.illness = illness;
+  this.doctors = [];
 }
 
-function Doctor(patient) {
-  this.patient = patient;
+function Doctor(first, last, picture) {
+  this.firstName = first;
+  this.lastName = last;
+  this.picture = picture;
 }
 
+function addDoctors() {
+  this.doctors = potentialDocs;
+}
+
+var potentialDocs = [];
 
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
+
+
+Patient.prototype.getDoctors = function(newPatient, print) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + this.illness + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
-      console.log(medicalIssue);
+      for(var i = 0; i < result.data.length; i++) {
+        var doctor = new Doctor(result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.image_url);
+        potentialDocs.push(doctor);
+        print(doctor.firstName, doctor.lastName, doctor.picture);
+      }
+      newPatient.doctors = potentialDocs;
+      console.log(newPatient, "inside");
     })
    .fail(function(error){
       console.log("fail");
     });
+    //.this is current patient object
 };
 
 exports.Doctor = Doctor;
